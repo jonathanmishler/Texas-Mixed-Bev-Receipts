f80a586b0480bc2ac1969c27d95df024aa07956f
Jonathan Mishler <jonathan.mishler@gmail.com>
2021-08-15 21:30:59 -0500

    create table and upload data

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     tx_app_token: str
+    db_name: str
+    db_user: str
+    db_password: str
+    db_host: str
 
     class Config:
         env_file = '.env'
